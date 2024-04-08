65ed84d461c712bfffac0dc408697187ab76b1a7
Sergii112 <srgkomlev@gmail.com>
2024-04-08 14:47:36 +0300

    add

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -31,7 +31,9 @@ test.describe("Auth", ()=>{
   })
 })
 /////////////////////////
+//
 
+import {test, expect} from "@playwright/test";
 test.describe.only("TODO MVC", ()=>{
   test.describe("TODO Create", ()=>{
 
@@ -41,7 +43,6 @@ test.describe.only("TODO MVC", ()=>{
 
     test('should allow me to add todo items', async ({ page }) => {
       const todoText = "Learn Playwright"
-
       await test.step("enter todo text and submit", async ()=>{
         const newTodo = page.getByPlaceholder('What needs to be done?');
         await newTodo.fill(todoText);
@@ -56,20 +57,24 @@ test.describe.only("TODO MVC", ()=>{
     });
   })
 })
+//
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+//
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+//
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
